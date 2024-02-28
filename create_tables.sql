a7150a9e0455eefcd6538609b15b306968ff04d5
“Ilia <student@netology.com>
2024-02-28 22:42:23 +0100

    Add SQL command to create tables

---
+++ create_tables.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id SERIAL PRIMARY KEY,
+    nickname VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+    album_id SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    YEAR DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS treks (
+    trek_id SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    lenght SMALLINT NOT NULL,
+    album_id SERIAL NOT NULL REFERENCES albums(album_id)
+);
+
+CREATE TABLE IF NOT EXISTS music_genres (
+    music_genre_id SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS collections (
+    collection_id SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    YEAR DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS artist_album (
+    artist_id SERIAL NOT NULL REFERENCES artists(artist_id),
+    album_id SERIAL NOT NULL REFERENCES albums(album_id),
+    CONSTRAINT PK_artist_album PRIMARY KEY (artist_id, album_id),
+    last_update TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS artist_music_genre (
+    artist_id SERIAL NOT NULL REFERENCES artists(artist_id),
+    music_genre_id SERIAL NOT NULL REFERENCES music_genres(music_genre_id),
+    CONSTRAINT PK_artist_music_genre PRIMARY KEY (artist_id, music_genre_id),
+    last_update TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS collection_trek (
+    collection_id SERIAL NOT NULL REFERENCES collections(collection_id),
+    trek_id SERIAL NOT NULL REFERENCES treks(trek_id),
+    CONSTRAINT PK_collection_trek PRIMARY KEY (collection_id, trek_id),
+    last_update TIMESTAMP
+);

c0947e6b38d5ccf1af09447b576324b1f65fdcfd
“Ilia <student@netology.com>
2024-03-20 21:22:27 +0100

    Lab with sqlalchemy libarary

---
+++ orm/client.py
@@ -0,0 +1,89 @@
+import json
+
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
+from models import create_tables, Publisher, Shop, Book, Stock, Sale
+
+DB_PORT = "5432"
+DB_NAME = "publisher"
+DB_HOST = "localhost"
+DB_ENGINE = "postgresql"
+DB_USERNAME = "postgres"
+DB_PASSWORD = "RtpZV7ow"
+
+DSN = "{}://{}:{}@{}:{}/{}".format(DB_ENGINE, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+engine = sqlalchemy.create_engine(DSN)
+
+create_table = False
+load_data = False
+
+
+class Connect:
+    def __init__(self):
+        self.Session = sessionmaker(bind=engine)
+        self.session = self.Session()
+
+
+def import_data(conn):
+    with open('fixtures/tests_data.json', 'r') as fd:
+        data = json.load(fd)
+
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+
+        conn.session.add(model(id=record.get('pk'), **record.get('fields')))
+        conn.session.commit()
+
+
+if __name__ == "__main__":
+    conn = Connect()
+
+    if create_table:
+        print(f"Connect to db: {DSN}")
+        create_tables(engine)
+    if load_data:
+        print("Load data:")
+        import_data(conn)
+
+    q_publisher = conn.session.query(Publisher).all()
+
+    for s in q_publisher:
+        print(f"{s.id} - {s.name}")
+
+    publisher_id = input("Please input ID a publisher: ")
+
+    q_purchase_invoices = conn.session.query(Book).join(Stock.shop).join(Stock.book).join(Stock.shop)\
+        .filter(Book.id_publisher == publisher_id)
+
+    for purchase_invoice in q_purchase_invoices.all():
+        print(purchase_invoice.id, purchase_invoice.title, purchase_invoice.id_publisher)
+
+# SQL query
+"""
+    SELECT
+    book.title as book_title,
+    shop.name as shop_name,
+    sale.price as price,
+    sale.date_sale as date
+    FROM book
+    JOIN stock ON stock.id_book = book.id
+    JOIN shop ON shop.id = stock.id_shop
+    JOIN sale ON sale.id_stock = stock.id
+    WHERE id_publisher = 1
+    ORDER BY shop.name
+"""
+
+# SQl result
+"""
+    book_title	                                shop_name	price	date
+    Programming Python, 4th Edition	            Amazon	    16.00	2018-10-25
+    Programming Python, 4th Edition	            Labirint	50.05	2018-10-25
+    Natural Language Processing with Python	    Labirint	50.05	2018-10-25
+"""
---
+++ orm/models.py
@@ -0,0 +1,61 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=50), nullable=False, unique=True)
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=100), nullable=False, unique=True)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+
+    publisher = relationship(Publisher, backref="publishers")
+
+
+class Shop(Base):
+    __tablename__ = "shop"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=50), nullable=False, unique=True)
+
+
+class Stock(Base):
+    __tablename__ = "stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    count = sq.Column(sq.Integer, nullable=False, unique=False)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+
+    book = relationship(Book, backref="books")
+    shop = relationship(Shop, backref="shops")
+
+
+class Sale(Base):
+    __tablename__ = "sale"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    count = sq.Column(sq.Integer, nullable=False)
+    price = sq.Column(sq.String(length=25), nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+
+    stock = relationship(Stock, backref="stocks")
+
+
+# for table in Base.metadata.tables.items():
+#     print("Table metadata...")
+#     print(table)

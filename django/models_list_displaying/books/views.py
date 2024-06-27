a26ca765fe2d994f4dbf5958e79f74e5bc7a8996
“Ilia <student@netology.com>
2024-06-27 13:58:13 +0200

    Add lab with django Books

--- django/models_list_displaying/books/views.py
+++ django/models_list_displaying/books/views.py
@@ -21,8 +21,6 @@ def pub_date(request, pub_date):
     books_objects = Book.objects.filter(pub_date=pub_date)
     books = [element for element in books_objects.values()]
 
-    print(books)
-
     paginator = Paginator(books, 25)
     current_page = request.GET.get('page', 1)
     page = paginator.get_page(current_page)
